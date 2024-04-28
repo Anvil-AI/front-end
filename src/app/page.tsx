7976075f7b2acc201e306e38225afdc846e5128c
KevinLandim <kevinb.landim@gmail.com>
2024-04-28 09:25:51 -0300

    adicionando textos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,16 +10,17 @@ export default function Home() {
 
       <GradientPage />
 
-      <div className="flex flex-1 justify-center items-center h-screen">
-        <h1 className="text-9xl"> Eva </h1>
-        <h2></h2>
+      <div className="flex flex-col flex-1 justify-center items-center h-screen">
+        <h1 className="text-9xl"> Eva</h1>
+        <p>Olá,usuario!</p>
+        <p>Sou a inteligência artificial que irá guiar seus estudos</p>
       </div>
 
       <div className="pb-6 flex justify-between">
 
         <div className="relative">
           <input type="text" className="bg-transparent border-b py-1 focus:outline-none focus:border-purple-600 focus:border-b-2 transition-colors peer" />
-          <label className="absolute left-0 top-1 text-gray-600 cursor-text peer-focus:text-xs peer-focus:-top-4 peer-focus:text-purple-600 transition-all">Username</label>
+          <label className="absolute left-0 top-1 text-gray-600 cursor-text peer-focus:text-xs peer-focus:-top-4 peer-focus:text-purple-600 transition-all">E-mail</label>
         </div>  
 
         <div className="pr-20">
@@ -27,7 +28,7 @@ export default function Home() {
           <input
             type="password" className="border-b bg-transparent focus:outline-none"/>
         </div>
-
+      
       </div>
 
     </div>
